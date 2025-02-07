e22702ba7302c2f5bcad76cb87ca7f0b8e59789d
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-02-07 21:38:37 +0530

    feature update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,23 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile, updateUserProfile, bookAppointment,listAppointment,cancelAppointment,paymentRazorpay,verifyRazorpay } from '../controllers/userController.js';
+import { 
+    registerUser, 
+    loginUser, 
+    getUserProfile, 
+    updateUserProfile, 
+    bookAppointment,
+    listAppointment,
+    cancelAppointment,
+    paymentRazorpay,
+    verifyRazorpay
+} from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
 import upload from '../middlewares/multer.js';
+
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
-
 
 userRouter.get('/get-profile', authUser, getUserProfile)
 userRouter.post('/update-profile', upload.single('image'), authUser, updateUserProfile)
@@ -16,5 +26,6 @@ userRouter.get("/appointments", authUser, listAppointment)
 userRouter.post("/cancel-appointment", authUser, cancelAppointment)
 userRouter.post("/payment-razorpay", authUser, paymentRazorpay)
 userRouter.post("/verifyRazorpay", authUser, verifyRazorpay)
+
 
 export default userRouter;
