35c289e7d724a8d20f322d75020d169ea20ba21f
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-02-12 12:51:08 +0530

    feature update

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, token, admintoken, X-Requested-With, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, token, admintoken, adminToken,X-Requested-With, Accept');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
     next();
